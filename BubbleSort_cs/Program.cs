7b4c302420e1f88de906d00f75ed977f7f48d5d5
patrick <kevinyangtianjin@gmail.com>
2014-06-15 02:07:46 +0800

    bubble commit

---
+++ BubbleSort_cs/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubble_sort_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] MyArray = { 9, 7, 11, 25, 3, 18, 0 };
+            Console.WriteLine("Initial Array: ");
+            BubbleSort(MyArray);
+            Console.ReadKey();
+
+        }
+        static void BubbleSort(int[] arr)
+        {
+            if (arr.Length <= 1)
+                return;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int CurrentMaxIndex = 0;
+                int temp = 0;
+                for (int n = 1; n < arr.Length - i; n++)
+                {
+                    if (arr[n] < arr[CurrentMaxIndex])
+                    {
+                        temp = arr[CurrentMaxIndex];
+                        arr[CurrentMaxIndex] = arr[n];
+                        arr[n] = temp;
+                        CurrentMaxIndex = n;
+                    }
+                    else
+                    {
+                        CurrentMaxIndex = n;
+                    }
+                }
+                PringIntArr(i + 1, arr);
+            }
+        }
+        /// <summary>
+        /// out put array
+        /// </summary>
+        /// <param name="arr"></param>
+        static void PringIntArr(int[] arr)
+        {
+            foreach (int e in arr)
+            {
+                Console.Write(e.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PringIntArr(int SortCounter, int[] arr)
+        {
+            Console.Write(SortCounter.ToString() + "        ");
+            foreach (int e in arr)
+            {
+                Console.Write(e.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+}
